835804523b65c60200fb6f8102e5dbd0d3441417
yjb23tech <yjb23tech@gmail.com>
2024-01-09 03:07:24 +0000

    Killers Win

--- App.jsx
+++ App.jsx
@@ -1,9 +1,11 @@
 import { createRoot } from "react-dom/client"; 
+import Card from "./components/Card.jsx"
 
 const App = () => {
     return (
         <div>
             <h1>Ruthless.</h1>
+            <Card />
         </div>
     )
 }
---
+++ components/Card.jsx
@@ -0,0 +1,10 @@
+const Card = (props) => {
+    return (
+        <div>
+            <h1>Image Goes Here</h1>
+            <h2>Song Content Goes Here</h2>
+        </div>
+    )
+}
+
+export default Card; 
